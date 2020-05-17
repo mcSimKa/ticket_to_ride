2a1a1660ceca399963865e5a2152a8aea5312503
mcSimKa <mcSimKa@users.noreply.github.com>
2020-05-17 15:28:56 +0300

    added tests

--- test_deck.py
+++ test_deck.py
@@ -3,12 +3,23 @@ from carddeck import CardDeck
 
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.card_deck = CardDeck()
+        self.card_deck.deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'D', 'K', 'A']
+
+    def test_deal_cards(self):
+        hand = self.card_deck.deal_cards(5)
+        self.assertEqual([2,3,4,5,6],hand)
+        self.assertEqual(8, len(self.card_deck.deck))
+
     def test_create_deck(self):
-        card_deck = CardDeck()
-        card_deck.deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'D', 'K', 'A']
-        hand = card_deck.deal_cards(5)
-        self.assertEqual([1,2,3,4,5],hand)
-        self.assertEqual(9, len(card_deck.deck))
+        self.assertEqual([2,3,4,5,6], self.card_deck.deck[0:5])
+        self.assertEqual(13, len(self.card_deck.deck))
+
+    def test_shuffle(self):
+        self.card_deck.shuffle()
+        self.assertNotEqual([2,3,4,5,6], self.card_deck.deck[0:5])
+        self.assertEqual(13, len(self.card_deck.deck))
 
 
 if __name__ == '__main__':
